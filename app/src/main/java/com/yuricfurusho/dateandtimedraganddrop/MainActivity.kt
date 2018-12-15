8225df011f5e7e8be2689a489242c4880da3449d
Yuri Furusho <yuricfurusho@gmail.com>
2018-12-15 10:40:58 -0200

    Closes #3 Create Initial Blank Activity

---
+++ app/src/main/java/com/yuricfurusho/dateandtimedraganddrop/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.yuricfurusho.dateandtimedraganddrop
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.yuricfurusho.dateandtimedraganddrop.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
